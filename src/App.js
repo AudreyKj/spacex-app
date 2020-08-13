3b8a1861c5dc79500686341e4fefc8702b1db1ab
AUDREY KADJAR <audreykadjar@gmail.com>
2020-08-13 17:22:57 +0200

    added App component and component for the launches data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import "./App.scss";
+import LaunchesData from "./LaunchesData.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              SpaceX - search & find launches
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </header>
+      <LaunchesData />
     </div>
   );
 }
---
+++ src/LaunchesData.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function LaunchesData() {
+  return (
+    <div className="search">
+      <form className="searchApps">
+        <label>
+          search launches:
+          <input type="text" name="launches" />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default LaunchesData;
