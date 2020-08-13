5299f31fa4c660b3ea0bd3e955f26015abe796bd
AUDREY KADJAR <audreykadjar@gmail.com>
2020-08-13 18:04:57 +0200

    added get request and basic styling to data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import "./App.scss";
 import LaunchesData from "./LaunchesData.js";
+import "./App.scss";
 
 function App() {
   return (
--- src/LaunchesData.js
+++ src/LaunchesData.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function LaunchesData() {
+  const [data, setData] = useState("");
+
+  /* eslint-disable */
+  //eslint disabled to pass empty array to useEffect
+  useEffect(() => {
+    axios
+      .get("https://api.spacexdata.com/v4/launches")
+      .then(res => {
+        console.log("res.data", res.data);
+        setData(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
-    <div className="search">
-      <form className="searchApps">
-        <label>
-          search launches:
-          <input type="text" name="launches" />
-        </label>
-      </form>
-    </div>
+    <section className="launchesData">
+      <div className="search">
+        <form className="searchApps">
+          <label>
+            search launches:
+            <input type="text" name="launches" />
+          </label>
+        </form>
+      </div>
+      <div className="result">
+        {data &&
+          data.map(launch => (
+            <div className="launch" key={launch.id}>
+              <div className="details">
+                <span className="details"> NAME: {launch.name} </span>
+                <span className="details">
+                  STATUS:
+                  {launch.success ? "launch was successful" : "launch failed"}
+                </span>
+                <span className="details"> DETAILS: {launch.details} </span>
+                <a className="details" href={launch.links.article}>
+                  LINK TO ARTICLE
+                </a>
+              </div>
+              <img
+                className="ship-image"
+                src={launch.links.patch.small}
+                alt="ship-image"
+              />
+            </div>
+          ))}
+      </div>
+    </section>
   );
 }
 
