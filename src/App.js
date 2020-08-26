6a36944f76dc8db1b82e109bd73c33a1a51cec3e
AUDREY KADJAR <audreykadjar@gmail.com>
2020-08-26 15:56:24 +0200

    fixed typo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, NavLink, useHistory } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
